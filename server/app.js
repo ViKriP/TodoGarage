9a08ea13e58401f7a974f88ef289c99671e685ee
ViKriP <viktor_biz@ukr.net>
2018-08-21 16:52:11 +0300

    port

--- server/app.js
+++ server/app.js
@@ -258,4 +258,8 @@ app.post('/api/task/deleteTask', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log('TodoLists server running on port 3000!'))
+//app.listen(3000, () => console.log('TodoLists server running on port 3000!'))
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
