71e671a1372097437a22ef3ab7b9f0952b95e887
ViKriP <viktor_biz@ukr.net>
2018-08-28 17:25:56 +0300

    p22

--- server/app.js
+++ server/app.js
@@ -15,12 +15,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 
 //app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
 //  res.sendFile(path.resolve('../' + __dirname, 'index.html'));
   res.send('Hello World!');
 });
-/*
+
 app.get('*', (req, res) =>{
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
