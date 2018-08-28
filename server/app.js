087b3f84de51d4c4ab8bd7c3c3237719a11872f3
ViKriP <viktor_biz@ukr.net>
2018-08-28 16:07:56 +0300

    p18

--- server/app.js
+++ server/app.js
@@ -16,13 +16,13 @@ const Task = require('./model/task');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 
-/*app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve('../' + __dirname, 'index.html'));
 });
-
+/*
 app.get('*', (req, res) =>{
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
